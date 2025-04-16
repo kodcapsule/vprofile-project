d3775e083c28030ba7e2eb0dfc2452e40d0ed8fa
Kuseh <kusehsw@gmail.com>
2025-04-16 15:13:47 +0000

    updated nexus set-up script

--- userdata/nexus-setup.sh
+++ userdata/nexus-setup.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
+sudo yum update -y
+sudo yum install wget -y
+sudo yum install java-17-openjdk -y
+sudo mkdir /app && cd /app
 
-sudo rpm --import https://yum.corretto.aws/corretto.key
-sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+sudo wget -O nexus.tar.gz  https://download.sonatype.com/nexus/3/nexus-3.79.1-04-linux-x86_64.tar.gz
 
-sudo yum install -y java-17-amazon-corretto-devel wget -y
+sudo tar -xvf nexus.tar.gz
+sudo adduser nexus
+sudo chown -R nexus:nexus /app/nexus
+sudo chown -R nexus:nexus /app/sonatype-work
 
-mkdir -p /opt/nexus/
-mkdir -p /tmp/nexus/
-cd /tmp/nexus/
-NEXUSURL="https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.78.0-14.tar.gz"
-wget $NEXUSURL -O nexus.tar.gz
-sleep 10
-EXTOUT=`tar xzvf nexus.tar.gz`
-NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
-sleep 5
-rm -rf /tmp/nexus/nexus.tar.gz
-cp -r /tmp/nexus/* /opt/nexus/
-sleep 5
-useradd nexus
-chown -R nexus.nexus /opt/nexus
+#sudo vi  /app/nexus/bin/nexus.rc
+
 cat <<EOT>> /etc/systemd/system/nexus.service
 [Unit]
 Description=nexus service
@@ -27,8 +21,10 @@ After=network.target
 [Service]
 Type=forking
 LimitNOFILE=65536
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop
+User=nexus
+Group=nexus
+ExecStart=/app/nexus/bin/nexus start
+ExecStop=/app/nexus/bin/nexus stop
 User=nexus
 Restart=on-abort
 
@@ -37,7 +33,8 @@ WantedBy=multi-user.target
 
 EOT
 
-echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
-systemctl daemon-reload
-systemctl start nexus
-systemctl enable nexus
+sudo chkconfig nexus on
+
+sudo systemctl start nexus
+
+cat /app/sonatype-work/nexus3/admin.password
